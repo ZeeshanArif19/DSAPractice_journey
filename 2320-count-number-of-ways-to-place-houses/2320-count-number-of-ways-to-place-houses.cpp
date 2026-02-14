0ddad209caedc08d299c30bd4c1c34432e5500f5
ZeeshanArif19 <149928037+ZeeshanArif19@users.noreply.github.com>
2026-02-14 20:20:21 +0530

    Time: 9 ms (27.03%), Space: 13.4 MB (40.94%) - LeetHub

---
+++ 2320-count-number-of-ways-to-place-houses/2320-count-number-of-ways-to-place-houses.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int MOD=1e9+7;
+    int fibo(long long n,vector<int> &dp){
+        if(n==0) return 1;
+        if(n==1) return 2;
+        if(dp[n]!=-1) return dp[n];
+        
+        return dp[n]=(fibo(n-1,dp)+fibo(n-2,dp))%MOD;
+        
+    }
+    int countHousePlacements(int n) {
+        vector<int> dp(n+1,-1);
+        long long oneway=fibo(n,dp);
+        return (oneway*oneway)%MOD;
+    }
+};
