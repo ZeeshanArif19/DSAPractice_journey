dfca8c01d85069c4dd75eb01c120c3267bb95e1c
ZeeshanArif19 <149928037+ZeeshanArif19@users.noreply.github.com>
2026-02-15 13:53:52 +0530

    Time: 0 ms (100%), Space: 10.5 MB (61.96%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> dp(n+2,0);
+        
+        for(int i=n-1;i>=0;i--){
+            int steal=nums[i]+dp[i+2];
+            int skip=dp[i+1];
+            dp[i]=max(steal,skip);
+        }
+        return dp[0];
+    }
+};
