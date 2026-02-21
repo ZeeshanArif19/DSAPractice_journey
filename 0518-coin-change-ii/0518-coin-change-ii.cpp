6211c82696bdc0dd7b80afeb1cc12bf105b7cb7a
ZeeshanArif19 <149928037+ZeeshanArif19@users.noreply.github.com>
2026-02-21 11:33:05 +0530

    Time: 7 ms (85.07%), Space: 10.2 MB (91.38%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -13,25 +13,35 @@ public:
     int change(int amount, vector<int>& coins) {
         int n=coins.size();
         // vector<vector<unsigned long long>> dp(n+1,vector<unsigned long long>(amount+1,0));
-        vector<unsigned long long> prev(amount+1);
-        vector<unsigned long long> curr(amount+1);
+        // vector<unsigned long long> prev(amount+1);
+        // vector<unsigned long long> curr(amount+1);
 
         // int res=solve(coins,dp,0,amount);
         // return res;
 
         // for(int i=0;i<n;i++) dp[i][0]=1;
-        curr[0]=1;
+        // curr[0]=1;
+        // for(int i=n-1;i>=0;i--){
+        //     for(int j=1;j<=amount;j++){
+        //         unsigned long long pick=0;
+        //         if(j>=coins[i]){
+        //             pick=curr[j-coins[i]];
+        //         }
+        //         unsigned long long notpick=prev[j];
+        //         curr[j]=pick+notpick;
+        //     }
+        //     prev=curr;
+        // }
+        // return prev[amount];
+
+        //1d optimization
+        vector<unsigned long long> dp(amount+1,0);
+        dp[0]=1;
         for(int i=n-1;i>=0;i--){
-            for(int j=1;j<=amount;j++){
-                unsigned long long pick=0;
-                if(j>=coins[i]){
-                    pick=curr[j-coins[i]];
-                }
-                unsigned long long notpick=prev[j];
-                curr[j]=pick+notpick;
+            for(int j=coins[i];j<=amount;j++){
+                dp[j]+=dp[j-coins[i]];
             }
-            prev=curr;
         }
-        return prev[amount];
+        return dp[amount];
     }
 };
