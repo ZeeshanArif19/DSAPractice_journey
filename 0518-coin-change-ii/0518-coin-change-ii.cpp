4b66cd5c7b2b494f1290b5717dcb0c46cc22bc83
ZeeshanArif19 <149928037+ZeeshanArif19@users.noreply.github.com>
2026-02-21 10:51:16 +0530

    Time: 22 ms (72.39%), Space: 10.8 MB (73.02%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int solve(vector<int>& coins,vector<vector<long long>> &dp,int i,int amount){
+        if(i==coins.size() || amount<0) return 0;
+        if(amount==0) return 1;
+        if(dp[i][amount]!=-1) return dp[i][amount];
+
+        long long pick=solve(coins,dp,i,amount-coins[i]);
+        long long notpick=solve(coins,dp,i+1,amount);
+
+        return dp[i][amount]=pick+notpick;
+    }
+    int change(int amount, vector<int>& coins) {
+        int n=coins.size();
+        // vector<vector<unsigned long long>> dp(n+1,vector<unsigned long long>(amount+1,0));
+        vector<unsigned long long> prev(amount+1);
+        vector<unsigned long long> curr(amount+1);
+
+        // int res=solve(coins,dp,0,amount);
+        // return res;
+
+        // for(int i=0;i<n;i++) dp[i][0]=1;
+        curr[0]=1;
+        for(int i=n-1;i>=0;i--){
+            for(int j=1;j<=amount;j++){
+                unsigned long long pick=0;
+                if(j>=coins[i]){
+                    pick=curr[j-coins[i]];
+                }
+                unsigned long long notpick=prev[j];
+                curr[j]=pick+notpick;
+            }
+            prev=curr;
+        }
+        return prev[amount];
+    }
+};
