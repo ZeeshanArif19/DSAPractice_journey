306496ff0f53db9e3e71407b6dec093f464b0015
ZeeshanArif19 <149928037+ZeeshanArif19@users.noreply.github.com>
2026-03-02 11:42:46 +0530

    Time: 0 ms (100%), Space: 38.2 MB (95.34%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -3,20 +3,30 @@ public:
     int leastInterval(vector<char>& tasks, int n) {
         int total_tasks=tasks.size();
         int maxf=0;
+        int count_maxf=0;
         vector<int> freq(26,0);
 
+        // for(char &it:tasks){
+        //     freq[it-'A']++;
+        //     maxf=max(maxf,freq[it-'A']);
+        // }
+        // for(int &it:freq){
+        //     if(it==maxf) count_maxf++;
+        // }
+        //one pass soln
         for(char &it:tasks){
             freq[it-'A']++;
-            maxf=max(maxf,freq[it-'A']);
+            if(freq[it-'A']>maxf){
+                maxf=freq[it-'A'];
+                count_maxf=1;
+            }
+            else if(maxf==freq[it-'A']){
+                count_maxf++;
+            }
         }
-
-        int count_maxf=0;
-        for(int &it:freq){
-            if(it==maxf) count_maxf++;
-        }
-
         int idle=(maxf-1)*(n+1)+count_maxf;
 
         return max(total_tasks,idle); //max(idle,busy)
+
     }
 };
