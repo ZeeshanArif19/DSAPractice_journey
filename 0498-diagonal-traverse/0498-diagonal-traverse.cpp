974e6584b1427df6cbcf93fca4231c8f2371b9f0
ZeeshanArif19 <149928037+ZeeshanArif19@users.noreply.github.com>
2026-03-18 12:40:59 +0530

    Time: 3 ms (52.5%), Space: 27.1 MB (26.87%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.cpp
+++ 0498-diagonal-traverse/0498-diagonal-traverse.cpp
@@ -2,22 +2,29 @@ class Solution {
 public:
     vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
         int m=mat.size();
-        unordered_map<int,vector<int>> mpp;  //sum of i+j ---> list of val
+        int n=mat[0].size();
+        // unordered_map<int,vector<int>> mpp;  //sum of i+j ---> list of val
+        vector<vector<int>> buckets(m+n-1);
         int maxsum=0;
 
         for(int i=0;i<m;i++){
-            for(int j=0;j<mat[i].size();j++){
-                mpp[i+j].push_back(mat[i][j]);
+            for(int j=0;j<n;j++){
+                buckets[i+j].push_back(mat[i][j]);
                 maxsum=max(maxsum,i+j);
             }
         }
 
         vector<int> res;
         for(int s=0;s<=maxsum;s++){
-            if(s%2==0) reverse(mpp[s].begin(),mpp[s].end());
-
-            for(int &val:mpp[s]){
-                res.push_back(val);
+            if(s%2==0){
+                for(int i=buckets[s].size()-1;i>=0;i--){
+                    res.push_back(buckets[s][i]);
+                }
+            }
+            else{
+                for(int i=0;i<buckets[s].size();i++){
+                    res.push_back(buckets[s][i]);
+                }
             }
         }
         return res;
