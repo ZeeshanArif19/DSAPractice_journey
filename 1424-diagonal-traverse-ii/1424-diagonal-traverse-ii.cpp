8cced5488d43a8a2a613331451e14664117a997a
ZeeshanArif19 <149928037+ZeeshanArif19@users.noreply.github.com>
2026-03-18 12:18:05 +0530

    Time: 98 ms (47.57%), Space: 101.6 MB (61.89%) - LeetHub

--- 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.cpp
+++ 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.cpp
@@ -1,27 +1,45 @@
 class Solution {
 public:
     vector<int> findDiagonalOrder(vector<vector<int>>& nums) {
+        // int m=nums.size();
+        // //numbers with equal sums of row and column indexes belong to the same diagonal.
+        // vector<array<int,3>> sumTup; //store them in form {sum,row,val}
+        // for(int i=0;i<m;i++){
+        //     int n=nums[i].size();
+        //     for(int j=0;j<n;j++){
+        //         sumTup.push_back({i+j,i,nums[i][j]});
+        //     }
+        // }
+
+        // auto cmp=[&](array<int,3>& a,array<int,3>& b){ //sort them on sum if sum equal-->sort row DESC
+        //     if(a[0]==b[0]) return a[1]>b[1];
+        //     return a[0]<b[0];
+        // };
+        // sort(sumTup.begin(),sumTup.end(),cmp);
+
+        // vector<int> res;
+        // for(int i=0;i<sumTup.size();i++){
+        //     res.emplace_back(sumTup[i][2]);
+        // }
+
+        // return res;
+
         int m=nums.size();
-        //numbers with equal sums of row and column indexes belong to the same diagonal.
-        vector<array<int,3>> sumTup; //store them in form {sum,row,val}
-        for(int i=0;i<m;i++){
-            int n=nums[i].size();
-            for(int j=0;j<n;j++){
-                sumTup.push_back({i+j,i,nums[i][j]});
+        unordered_map<int,vector<int>> mpp;  //sum of indices-->list of values
+        int maxsum=0;
+
+        for(int i=m-1;i>=0;i--){   //bottom up traversal so that row is already in DESC
+            for(int j=0;j<nums[i].size();j++){
+                mpp[i+j].push_back(nums[i][j]);
+                maxsum=max(maxsum,i+j);
             }
         }
-
-        auto cmp=[&](array<int,3>& a,array<int,3>& b){ //sort them on sum if sum equal-->sort row DESC
-            if(a[0]==b[0]) return a[1]>b[1];
-            return a[0]<b[0];
-        };
-        sort(sumTup.begin(),sumTup.end(),cmp);
-        
         vector<int> res;
-        for(int i=0;i<sumTup.size();i++){
-            res.emplace_back(sumTup[i][2]);
+        for(int s=0;s<=maxsum;s++){
+            for(int &val:mpp[s]){
+                res.push_back(val);
+            }
         }
-
         return res;
     }
 };
