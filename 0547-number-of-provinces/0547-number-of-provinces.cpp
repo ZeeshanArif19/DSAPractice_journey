828831a2b074f6b909a871dc5402db08895ac8c8
ZeeshanArif19 <149928037+ZeeshanArif19@users.noreply.github.com>
2026-04-06 12:33:24 +0530

    Time: 4 ms (26.92%), Space: 19.6 MB (62.57%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,31 +1,48 @@
-class Solution {
-public:
-    void bfs(int node,unordered_map<int,bool> &visited,vector<vector<int>> &isConnected){
-        queue<int> q;
-        q.push(node);
-        visited[node]=true;
-        while(!q.empty()){
-            int front=q.front();
-            q.pop();
-            for(int j=0;j<isConnected.size();j++){
-                if(isConnected[front][j]==1 && !visited[j]){
-                    visited[j]=true;
-                    q.push(j);
-                }
+class DSU{
+    public:
+    vector<int> parent;
+    vector<int> size;
+    DSU(int n){
+        parent.resize(n);
+        size.resize(n,1);
+        for(int i=0;i<n;i++){
+            parent[i]=i;
+        }
+    }
+    int find(int x){
+        if(x==parent[x]) return x;
+        return parent[x]=find(parent[x]);
+    }
+    void unite(int x,int y){
+        int rootx=find(x);
+        int rooty=find(y);
+        if(rootx!=rooty){
+            if(size[rootx]<size[rooty]){
+                parent[rootx]=rooty;
+                size[rooty]+=size[rootx];
+            }
+            else{
+                parent[rooty]=rootx;
+                size[rootx]+=size[rooty];
             }
         }
     }
+};
+class Solution {
+public:
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n=isConnected.size();
-        int count=0;
-        unordered_map<int,bool> visited;
-        
+        DSU ds(n);
+        int components=n;
+
         for(int i=0;i<n;i++){
-            if(!visited[i]){
-                count++;
-                bfs(i,visited,isConnected);
+            for(int j=0;j<n;j++){
+                if(isConnected[i][j]==1 && ds.find(i)!=ds.find(j)){
+                    ds.unite(i,j);
+                    components--;
+                }
             }
         }
-        return count;
+        return components;
     }
 };
