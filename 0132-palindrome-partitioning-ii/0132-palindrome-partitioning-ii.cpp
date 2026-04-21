3bbf2a7bdda0eae1d0fcbe50457b7462724b1eec
ZeeshanArif19 <149928037+ZeeshanArif19@users.noreply.github.com>
2026-04-21 15:36:45 +0530

    Time: 50 ms (67.51%), Space: 12.1 MB (44.9%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -1,36 +1,46 @@
 class Solution {
 public:
-    bool isPalindrome(string &temp,int i,int j){
-        while(i<j){
-            if(temp[i]!=temp[j]) return false;
-            i++;
-            j--;
-        }
-        return true;
-    }
-    int solve(string &s,int i,int n,vector<int> &dp){
-        if(i==n) return 0;
-        if(dp[i]!=-1) return dp[i];
+    // bool isPalindrome(string &temp,int i,int j){
+    //     while(i<j){
+    //         if(temp[i]!=temp[j]) return false;
+    //         i++;
+    //         j--;
+    //     }
+    //     return true;
+    // }
+    // int solve(string &s,int i,int n,vector<int> &dp){
+    //     if(i==n) return 0;
+    //     if(dp[i]!=-1) return dp[i];
 
-        int mincost=1e9;
-        for(int j=i;j<n;j++){
-            if(isPalindrome(s,i,j)){
-                int cost=1+solve(s,j+1,n,dp);
-                mincost=min(mincost,cost);
-            }
-        }
-        return dp[i]=mincost;  
-    }
+    //     int mincost=1e9;
+    //     for(int j=i;j<n;j++){
+    //         if(isPalindrome(s,i,j)){
+    //             int cost=1+solve(s,j+1,n,dp);
+    //             mincost=min(mincost,cost);
+    //         }
+    //     }
+    //     return dp[i]=mincost;  
+    // }
     int minCut(string s) {
         int n=s.size();
+        vector<vector<bool>> isPal(n+1,vector<bool>(n+1,false));
         vector<int> dp(n+1);
         // return solve(s,0,n,dp) - 1; //because the code does a partition at the very end of the string
+
+        for(int i=n-1;i>=0;i--){
+            for(int j=i;j<n;j++){
+                if(s[i]==s[j]){
+                    if(j-i<=2) isPal[i][j]=true;
+                    else isPal[i][j]=isPal[i+1][j-1];
+                }
+            }
+        }
 
         dp[n]=0;
         for(int i=n-1;i>=0;i--){
             int mincost=1e9;
             for(int j=i;j<n;j++){
-                if(isPalindrome(s,i,j)){
+                if(isPal[i][j]){
                     int cost=1+dp[j+1];
                     mincost=min(cost,mincost);
                 }
