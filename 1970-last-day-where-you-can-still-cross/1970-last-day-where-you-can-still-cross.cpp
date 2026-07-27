4c9eee8f633dc389ebec7bc87e1be49858d895f8
ZeeshanArif19 <149928037+ZeeshanArif19@users.noreply.github.com>
2026-07-28 02:32:37 +0530

    Time: 969 ms (9.48%), Space: 398.2 MB (5.09%) - LeetHub

---
+++ 1970-last-day-where-you-can-still-cross/1970-last-day-where-you-can-still-cross.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    bool isValid(vector<vector<int>>& water_time,int row,int col,int mid){
+        vector<vector<bool>> vis(row,vector<bool>(col,false));
+        int dr[4]={1,-1,0,0};
+        int dc[4]={0,0,1,-1};
+        
+        queue<vector<int>> q;
+        for(int j=0;j<col;j++){
+            if(water_time[0][j]>mid){
+                q.push({0,j});
+                vis[0][j]=true;
+            }
+        }
+
+        while(!q.empty()){
+            auto front=q.front();
+            q.pop();
+            int i=front[0];
+            int j=front[1];
+            
+            if(i==row-1) return true;
+
+            for(int k=0;k<4;k++){
+                int nr=i+dr[k];
+                int nc=j+dc[k];
+                if(nr>=0 && nr<row && nc>=0 && nc<col){
+                    if(water_time[nr][nc]>mid && !vis[nr][nc]){
+                        q.push({nr,nc});
+                        vis[nr][nc]=true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+    int latestDayToCross(int row, int col, vector<vector<int>>& cells) {
+
+        vector<vector<int>> water_time(row,vector<int>(col,0));
+        int time=1;
+
+        for(int i=0;i<cells.size();i++){
+            int u=cells[i][0]-1;
+            int v=cells[i][1]-1;
+            water_time[u][v]=time;
+            time++;
+        }
+
+        int l=1;
+        int r=row*col;
+        int ans=-1;
+
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(isValid(water_time,row,col,mid)){
+                ans=mid;
+                l=mid+1;
+            }
+            else r=mid-1;
+        }
+        return ans;
+    }
+};
